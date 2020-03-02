d1321d151833aa448b25635b4bc73d446097dbaf
Jack <jr00856@surrey.ac.uk>
2020-03-02 11:36:50 +0000

    player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //speed of player
+    public float speed = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 move = new Vector2(horizontal, vertical);
+        Vector2 position = transform.position;
+        position = position + move*speed*Time.deltaTime;
+        transform.position = position;
+    }
+}
