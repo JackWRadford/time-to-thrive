6b396c131165967630adf90a57c6db6427d40bc3
Jack <jr00856@surrey.ac.uk>
2020-03-04 17:14:54 +0000

    player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     //player animation
     Animator animator;
     Vector2 lookDirection = new Vector2(1,0);
+    Vector2 move = new Vector2(1,0);
 
     void Start()
     {
@@ -22,19 +23,22 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector2 move = new Vector2(horizontal, vertical);
+        move = new Vector2(horizontal, vertical);
 
         if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             lookDirection.Set(move.x, move.y);
             lookDirection.Normalize();
         }
-        animator.SetFloat("Look X", lookDirection.x);
-        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Horizontal", lookDirection.x);
+        animator.SetFloat("Vertical", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
+    }
 
+    void FixedUpdate()
+    {
         Vector2 position = transform.position;
-        position = position + move*speed*Time.deltaTime;
+        position = position + move*speed*Time.fixedDeltaTime;
         transform.position = position;
     }
 }
