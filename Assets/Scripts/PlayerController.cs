95cda77cb441e180f3c122366f92a36cc32d56d7
Jack <jr00856@surrey.ac.uk>
2020-03-04 13:38:32 +0000

    left anim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,32 @@ public class PlayerController : MonoBehaviour
     //speed of player
     public float speed = 3.0f;
 
+    //player animation
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
         Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         Vector2 position = transform.position;
         position = position + move*speed*Time.deltaTime;
         transform.position = position;
