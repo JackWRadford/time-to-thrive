b1e32f06e5fc235d1d289e8cf6ac1df24ff737c6
Jack <jr00856@surrey.ac.uk>
2020-03-04 21:58:55 +0000

    better walking anim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //speed of player
-    public float speed = 3.0f;
+    public float speed = 3.0f;//speed of player
 
-    //player animation
     Animator animator;
-    Vector2 lookDirection = new Vector2(1,0);
+    Rigidbody2D rigidbody2D;
+    Vector2 lookDirection = new Vector2(1,0);//direction player is moving
     Vector2 move = new Vector2(1,0);
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -24,8 +24,9 @@ public class PlayerController : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         move = new Vector2(horizontal, vertical);
+        move.Normalize();//stop increased speed diagonally
 
-        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))//if moving
         {
             lookDirection.Set(move.x, move.y);
             lookDirection.Normalize();
@@ -34,11 +35,11 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("Vertical", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
     }
-
+    //use to stop framerate issues
     void FixedUpdate()
     {
-        Vector2 position = transform.position;
+        Vector2 position = rigidbody2D.position;//change position
         position = position + move*speed*Time.fixedDeltaTime;
-        transform.position = position;
+        rigidbody2D.MovePosition(position);
     }
 }
