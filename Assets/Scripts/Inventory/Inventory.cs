7ebe23451c38da6c63b3927a05c7d108a70fa07b
JackR <jack.radford@live.co.uk>
2020-03-08 13:47:39 +0000

    basic inventory

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    #region Singleton 
+    public static Inventory instance;
+
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.Log("more than one instance of inventory found");
+        }
+        instance = this;
+    }
+
+    #endregion
+
+    //update UI when certain events happen
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    public int space = 9;
+    public List<Item> items = new List<Item>();
+
+    public bool Add(Item item)
+    {
+        if(items.Count >= space)
+        {
+            return false;
+        }
+        items.Add(item);
+
+        //update UI
+        if(onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+        }
+
+        return true;
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+
+        //update UI
+        if(onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Inventory/PickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : Interactable
+{
+    //scriptable item to be added to inventory
+    public Item item;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player"))
+        {
+            //Add to inventory if enough space
+
+
+            Debug.Log("PickUp " + item.name);
+            bool wasPickedUp = Inventory.instance.Add(item);
+            if(wasPickedUp)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
