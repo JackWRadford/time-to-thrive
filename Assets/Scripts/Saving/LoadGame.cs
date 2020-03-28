e173aa5ef68df2f3d3c37af294ae353e93747fa6
JackR <jack.radford@live.co.uk>
2020-03-28 01:34:16 +0000

    added more UI and saving and loading player data

---
+++ Assets/Scripts/Saving/LoadGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadGame : MonoBehaviour
+{
+    void Start()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        GameObject.FindGameObjectWithTag("Player").transform.position = position;
+    }
+}
