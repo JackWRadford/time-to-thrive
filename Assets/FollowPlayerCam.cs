6313a22d1618ab8fc8da9b520d57c19faa6234d4
JackR <jack.radford@live.co.uk>
2020-04-08 15:13:08 +0100

    added playerFollowCamera script to CMVC

---
+++ Assets/FollowPlayerCam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class FollowPlayerCam : MonoBehaviour
+{
+    private GameObject player;
+    private CinemachineVirtualCamera virtualCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        player = GameObject.FindWithTag("Player");
+
+        virtualCamera.Follow = player.transform;
+        virtualCamera.LookAt = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
