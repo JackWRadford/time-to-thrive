3a170c43abaee2be0806f9c810e0016545b60570
JackR <jack.radford@live.co.uk>
2020-04-14 23:27:57 +0100

    added up and down fences

--- Assets/Scripts/PlaceableObjects/FenceController.cs
+++ Assets/Scripts/PlaceableObjects/FenceController.cs
@@ -25,6 +25,9 @@ public class FenceController : MonoBehaviour
                 if(hit.collider.GetComponent<FenceController>())
                 {
                     Debug.Log("Fence Up");
+                    hit.collider.GetComponent<FenceController>().down = true;
+                    hit.collider.GetComponent<FenceController>().UpdateConnections();
+                    this.up = true;
                 }
             }
         }
@@ -36,7 +39,9 @@ public class FenceController : MonoBehaviour
                 if(hit.collider.GetComponent<FenceController>())
                 {
                     Debug.Log("Fence Down");
-                    
+                    hit.collider.GetComponent<FenceController>().up = true;
+                    hit.collider.GetComponent<FenceController>().UpdateConnections();
+                    this.down = true;
                 }
             }
         }
@@ -74,28 +79,52 @@ public class FenceController : MonoBehaviour
     public Sprite CalcSprite()
     {
         string sprt = "simpleFence_0";
+        int f = 0;
         if((!up)&&(!down)&&(!left)&&(!right))
         {
             //no connections
-            sprt = "simpleFence_0";
+            //sprt = "simpleFence_0";
+            f = 0;
         }else if((!up)&&(!down)&&(left)&&(!right))
         {
             //left connection
-            sprt = "simpleFence_3";
+            //sprt = "simpleFence_3";
+            f = 3;
         }
         else if((!up)&&(!down)&&(!left)&&(right))
         {
             //right connection
-            sprt = "simpleFence_1";
+            //sprt = "simpleFence_1";
+            f = 1;
         }
         else if((!up)&&(!down)&&(left)&&(right))
         {
             //left and right connection
-            sprt = "simpleFence_2";
+            //sprt = "simpleFence_2";
+            f = 2;
         }
-        Sprite[] allFences = Resources.LoadAll<Sprite>("Sprites/Placeable/WoodFence");
+        else if((up)&&(!down)&&(!left)&&(!right))
+        {
+            //up connection
+            //sprt = "simpleFence_2";
+            f = 5;
+        }
+        else if((!up)&&(down)&&(!left)&&(!right))
+        {
+            //down connection
+            //sprt = "simpleFence_2";
+            f = 4;
+        }
+        else if((up)&&(down)&&(!left)&&(!right))
+        {
+            //up and down connection
+            //sprt = "simpleFence_2";
+            f = 6;
+        }
+        Sprite[] allFences = Resources.LoadAll<Sprite>("Sprites/Placeable/FenceWood");
 
-        return allFences.Single(s => s.name == sprt);
+        //return allFences.Single(s => s.name == sprt);
+        return allFences[f];
     }
 
     public void UpdateConnections()
