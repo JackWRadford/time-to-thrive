14cc836b36bc16e07803fbd2f3ab617d29485621
JackR <jack.radford@live.co.uk>
2020-04-20 23:44:27 +0100

    added basic object save (need to save more object data)

--- Assets/Scripts/GameManager/ObjectManager.cs
+++ Assets/Scripts/GameManager/ObjectManager.cs
@@ -6,13 +6,23 @@ using System.Linq;
 public class ObjectManager : MonoBehaviour
 {
     Dictionary<List<float>, string> objectsString = new Dictionary<List<float>, string>();
-    Dictionary<List<float>, GameObject> objectsGO = new Dictionary<List<float>, GameObject>();
+    //Dictionary<List<float>, GameObject> objectsGO = new Dictionary<List<float>, GameObject>();
+
+    void Awake()
+    {
+        GameEvents.SaveInitiated += Save;
+    }
+
+    void Start()
+    {
+        Load();
+    }
 
     public void AddObject(float x, float y, string title, GameObject go)
     {
         List<float> pos = new List<float>{x,y};
         objectsString.Add(pos, title);
-        objectsGO.Add(pos, go);
+        //objectsGO.Add(pos, go);
     }
 
     public void RemoveObject(float x, float y)
@@ -39,5 +49,32 @@ public class ObjectManager : MonoBehaviour
     {
         
         return false;
+    }
+
+    public void SpawnSavedObjects(Dictionary<List<float>, string> objects)
+    {
+        foreach (var obj in objects)
+        {
+            GameObject go = Resources.Load<GameObject>("Placeable/" + obj.Value);
+            Instantiate(go, new Vector3(obj.Key[0], obj.Key[1], 0), Quaternion.identity);
+            //populate list of objects
+            objectsString.Add(obj.Key, obj.Value);
+        }
+    }
+
+    void Save()
+    {
+        SaveSystem.Save<Dictionary<List<float>, string>>(objectsString, "Objects");
+        Debug.Log("saved objects");
+    }
+
+    void Load()
+    {
+        Debug.Log("try to objects");
+        if(SaveSystem.SaveExists("Objects"))
+        {
+            Debug.Log("Save Exists");
+            SpawnSavedObjects(SaveSystem.Load<Dictionary<List<float>, string>>("Objects"));
+        }
     }
 }
