e7ee2926f6707c35283fde01122781a826c962a4
JackR <jack.radford@live.co.uk>
2020-04-26 16:16:05 +0100

    added basic UI for drop and consume item

---
+++ Assets/Scripts/Inventory/ItemButtons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemButtons : MonoBehaviour
+{
+    private GameItem gameItem;
+
+    public void DropItem()
+    {
+
+    }
+
+    public void ConsumeItem()
+    {
+        
+    }
+}
