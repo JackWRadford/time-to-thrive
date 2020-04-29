9a04358b8450a19d5121bb736b41c9e79771af9a
JackR <jack.radford@live.co.uk>
2020-04-29 15:55:25 +0100

    added tent scene

---
+++ Assets/Scripts/PlaceableObjects/Tent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tent : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if player collides change scene to inside tent
+        if(collision.gameObject.tag == "Player")
+        {
+            //set player position to entrance of tent
+            
+            //enter tent (change scene)
+            SceneManager.LoadScene("Tent");
+        }
+    }
+}
