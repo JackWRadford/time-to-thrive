01582286535017639550c7c6d19c6534da76f199
JackR <jack.radford@live.co.uk>
2020-08-14 16:10:20 +0100

    added WorldData prefab, methods to get sub directories of /saves/, get worldName from whole path

--- Assets/Scripts/Menu/WorldsController.cs
+++ Assets/Scripts/Menu/WorldsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System.Text.RegularExpressions;
 
 public class WorldsController : MonoBehaviour
 {
@@ -13,6 +15,12 @@ public class WorldsController : MonoBehaviour
     void Awake()
     {
         passData = GameObject.Find("PassInfo").GetComponent<PassData>();
+
+        string[] worldDirectories = loadWorldData();
+        foreach (var dir in worldDirectories)
+        {
+            Debug.Log(GetWorldNameFromPath(dir));
+        }
     }
     
     //method to add new world
@@ -23,5 +31,26 @@ public class WorldsController : MonoBehaviour
             passData.SetWorldName(this.worldNameInputText.GetComponent<InputField>().text);
             Debug.Log(this.worldNameInputText.GetComponent<InputField>().text.ToString());
         }
+    }
+
+    //method to get worldName (data) from game save file
+    public string[] loadWorldData()
+    {
+        string path = Application.persistentDataPath + "/saves/";
+
+        //get save data and load into WorldDetails Prefabs
+        return Directory.GetDirectories(path);
+    }
+
+    //method to just get worldName from whole path
+    public string GetWorldNameFromPath(string path)
+    {
+        return Regex.Match(path, "[^/]+$").ToString();
+    }
+
+    //method to generate WorldData prefab
+    public void GenerateWorldDataUI()
+    {
+        
     }
 }
