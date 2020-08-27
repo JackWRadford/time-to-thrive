21350758ef63c01ef4737e080836a1069267d39e
JackR <jack.radford@live.co.uk>
2020-08-27 18:13:19 +0100

    added incrementing/decrementing and resetting light for midday and midnight

--- Assets/Scripts/Environment/Ambient.cs
+++ Assets/Scripts/Environment/Ambient.cs
@@ -14,8 +14,8 @@ public class Ambient : MonoBehaviour
     private string worldTime;
     private int daysPassed;
     private float timer = 0f;
-    public float delay = 1f;
-    private float currentHours = 00f;
+    public float delay = 0.1f;
+    private float currentHours = 12f;
     private float currentMinutes = 00f;
     private float currentSeconds = 00f;
     private StringBuilder sbTime = new StringBuilder("");
@@ -28,7 +28,7 @@ public class Ambient : MonoBehaviour
 
     void Start()
     {
-        globalLight.GetComponent<Light2D>().intensity = 1.0f;
+        globalLight.GetComponent<Light2D>().intensity = 1f;
     }
 
     void Update()
@@ -61,6 +61,24 @@ public class Ambient : MonoBehaviour
                 {
                     this.currentHours = 00f;
                     this.daysPassed++;
+
+                    //midnight
+                    ResetLight(true);
+                }
+                else if(this.currentHours == 12)
+                {
+                    //midday
+                    ResetLight(false);
+                }
+                else if(this.currentHours < 12)
+                {
+                    //increase light
+                    ChangeGlobalLight(true);
+                }
+                else if((this.currentHours > 12)&&(this.currentHours < 24))
+                {
+                    //decrease light
+                    ChangeGlobalLight(false);
                 }
             }
         }
@@ -75,5 +93,29 @@ public class Ambient : MonoBehaviour
         this.sbTime.Append(this.currentMinutes.ToString().PadLeft(2, '0'));
         this.sbTime.Append(":");
         this.sbTime.Append(this.currentSeconds.ToString().PadLeft(2, '0'));
+    }
+
+    public void ChangeGlobalLight(bool increase)
+    {
+        if(increase)
+        {
+            globalLight.GetComponent<Light2D>().intensity += 0.0667f;
+        }
+        else if(!increase)
+        {
+            globalLight.GetComponent<Light2D>().intensity -= 0.0667f;
+        }
+    }
+
+    public void ResetLight(bool night)
+    {
+        if(night)
+        {
+            globalLight.GetComponent<Light2D>().intensity = 0.2f;
+        }
+        else if(!night)
+        {
+            globalLight.GetComponent<Light2D>().intensity = 1f;
+        }
     }
 }
